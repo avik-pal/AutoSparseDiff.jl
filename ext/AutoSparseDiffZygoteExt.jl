5bf55c668e8fae13cc61c126a6b2c9093de2bda6
Avik Pal <avikpal@mit.edu>
2023-08-10 15:18:03 -0400

    Zygote use proper inplace accumulation

--- ext/AutoSparseDiffZygoteExt.jl
+++ ext/AutoSparseDiffZygoteExt.jl
@@ -8,9 +8,17 @@ function __gradient(::Union{AutoSparseZygote, AutoZygote}, f, x, cols)
     return vec(∂x)
 end
 
-function __jacobian!(J::AbstractMatrix, ::Union{AutoSparseZygote, AutoZygote}, f, x)
-    Jz = only(Zygote.jacobian(f, x))
-    J .= Jz
+# Zygote doesn't provide a way to accumulate directly into `J`. So we modify the code from
+# https://github.com/FluxML/Zygote.jl/blob/82c7a000bae7fb0999275e62cc53ddb61aed94c7/src/lib/grad.jl#L140-L157C4
+import Zygote: _jvec, _eyelike, _gradcopy!
+
+@views function __jacobian!(J::AbstractMatrix, ::Union{AutoSparseZygote, AutoZygote}, f, x)
+    y, back = Zygote.pullback(_jvec ∘ f, x)
+    δ = _eyelike(y)
+    for k in LinearIndices(y)
+        grad = only(back(δ[:, k]))
+        _gradcopy!(J[k, :], grad)
+    end
     return J
 end
 
